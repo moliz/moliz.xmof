954209535b1b97a8a7601193d76b1643e2738180
Philip Langer <p.langer@gmail.com>
2012-05-02 16:38:04 +0200

    Add launch configuration type.

---
+++ org.modelexecution.fumldebug.debugger.uml2/src/org/modelexecution/fumldebug/debugger/uml2/internal/launching/ActivityDiagramLaunchConfigurationDelegate.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012 Vienna University of Technology.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 which accompanies 
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Langer - initial API and implementation
+ */
+package org.modelexecution.fumldebug.debugger.uml2.internal.launching;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class ActivityDiagramLaunchConfigurationDelegate implements
+		ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		System.out.println(configuration);
+
+	}
+
+}
