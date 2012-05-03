58b4af292a57c932c70ae986ace66aea161a2631
Philip Langer <p.langer@gmail.com>
2012-05-03 12:27:00 +0200

    Add implementation for obtaining the configured activity launch delegate.

--- org.modelexecution.fumldebug.debugger.uml2/src/org/modelexecution/fumldebug/debugger/launch/ActivityLaunchDelegate.java
+++ org.modelexecution.fumldebug.debugger.uml2/src/org/modelexecution/fumldebug/debugger/launch/ActivityLaunchDelegate.java
@@ -9,20 +9,83 @@
  */
 package org.modelexecution.fumldebug.debugger.launch;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.modelexecution.fumldebug.debugger.ActivityProviderRegistry;
+import org.modelexecution.fumldebug.debugger.FUMLDebuggerPlugin;
+import org.modelexecution.fumldebug.debugger.IActivityProvider;
+
+import fUML.Syntax.Activities.IntermediateActivities.Activity;
 
 public class ActivityLaunchDelegate extends LaunchConfigurationDelegate {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		System.out.println(configuration);
 
+		Activity activity = loadActivity(configuration);
+
+		// TODO start process
+		System.out.println(activity);
+
+	}
+
+	private Activity loadActivity(ILaunchConfiguration configuration)
+			throws CoreException {
+		String resourcePath = getResourcePath(configuration);
+		String activityName = getActivityName(configuration);
+		Collection<Activity> allActivities = getAllActivities(resourcePath);
+		Activity activity = getActivityByName(allActivities, activityName);
+		return activity;
+	}
+
+	private String getActivityName(ILaunchConfiguration configuration)
+			throws CoreException {
+		return configuration.getAttribute(FUMLDebuggerPlugin.ATT_ACTIVITY_NAME,
+				(String) null);
+	}
+
+	private String getResourcePath(ILaunchConfiguration configuration)
+			throws CoreException {
+		return configuration.getAttribute(FUMLDebuggerPlugin.ATT_RESOURCE,
+				(String) null);
+	}
+
+	private Collection<Activity> getAllActivities(String resourcePath) {
+		Collection<Activity> allActivities = Collections.emptyList();
+		ActivityProviderRegistry activityProviderRegistry = ActivityProviderRegistry
+				.getInstance();
+		IResource iResource = loadResource(resourcePath);
+		if (iResource.exists()
+				&& activityProviderRegistry.hasActivityProvider(iResource)) {
+			IActivityProvider activityProvider = activityProviderRegistry
+					.getActivityProvider(iResource);
+			allActivities = activityProvider.getActivities(iResource);
+		}
+		return allActivities;
+	}
+
+	private IResource loadResource(String resourcePath) {
+		return ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(resourcePath);
+	}
+
+	private Activity getActivityByName(Collection<Activity> allActivities,
+			String activityName) {
+		for (Activity activity : allActivities) {
+			if (activityName.equals(activity.name)) {
+				return activity;
+			}
+		}
+		return null;
 	}
 
 }
