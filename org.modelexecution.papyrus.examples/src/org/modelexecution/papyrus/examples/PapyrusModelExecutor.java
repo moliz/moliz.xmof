7efdc763ca6b25ec44cada1f656983cbd85b89d6
Philip Langer <langer@big.tuwien.ac.at>
2013-02-06 08:20:29 +0100

    Adapts example to API changes

--- org.modelexecution.papyrus.examples/src/org/modelexecution/papyrus/examples/PapyrusModelExecutor.java
+++ org.modelexecution.papyrus.examples/src/org/modelexecution/papyrus/examples/PapyrusModelExecutor.java
@@ -26,7 +26,7 @@ import org.modelexecution.fuml.convert.IConverter;
 import org.modelexecution.fumldebug.core.ExecutionContext;
 import org.modelexecution.fumldebug.core.ExecutionEventListener;
 import org.modelexecution.fumldebug.core.event.Event;
-import org.modelexecution.fumldebug.core.event.StepEvent;
+import org.modelexecution.fumldebug.core.event.SuspendEvent;
 import org.modelexecution.fumldebug.debugger.papyrus.util.DiResourceUtil;
 
 import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValueList;
@@ -165,16 +165,17 @@ public class PapyrusModelExecutor {
 					@Override
 					public void notify(Event event) {
 						System.out.println(event);
-						if (event instanceof StepEvent) {
-							StepEvent stepEvent = (StepEvent) event;
+						if (event instanceof SuspendEvent) {
+							SuspendEvent suspendEvent = (SuspendEvent) event;
 							getExecutionContext().resume(
-									stepEvent.getActivityExecutionID());
+									suspendEvent.getActivityExecutionID());
 						}
 					}
 				});
 
 		// start the execution
-		getExecutionContext().debug(activity, null, new ParameterValueList());
+		getExecutionContext().executeStepwise(activity, null,
+				new ParameterValueList());
 	}
 
 	/**
