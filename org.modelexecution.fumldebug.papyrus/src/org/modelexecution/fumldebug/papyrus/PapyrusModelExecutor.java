3ca7ccfcc6bcc9df68075a9ad64272e6033db729
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2013-06-27 10:20:58 +0200

    Adds replacement of primitive behaviors for execution of Papyrus models

--- org.modelexecution.fumldebug.papyrus/src/org/modelexecution/fumldebug/papyrus/PapyrusModelExecutor.java
+++ org.modelexecution.fumldebug.papyrus/src/org/modelexecution/fumldebug/papyrus/PapyrusModelExecutor.java
@@ -10,6 +10,8 @@
 package org.modelexecution.fumldebug.papyrus;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -31,7 +33,13 @@ import org.modelexecution.fumldebug.papyrus.util.DiResourceUtil;
 
 import fUML.Semantics.Classes.Kernel.Object_;
 import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValueList;
+import fUML.Syntax.Actions.BasicActions.CallBehaviorAction;
+import fUML.Syntax.Activities.CompleteStructuredActivities.StructuredActivityNode;
 import fUML.Syntax.Activities.IntermediateActivities.Activity;
+import fUML.Syntax.Activities.IntermediateActivities.ActivityNode;
+import fUML.Syntax.Activities.IntermediateActivities.DecisionNode;
+import fUML.Syntax.CommonBehaviors.BasicBehaviors.Behavior;
+import fUML.Syntax.CommonBehaviors.BasicBehaviors.OpaqueBehavior;
 
 public class PapyrusModelExecutor {
 
@@ -67,9 +75,58 @@ public class PapyrusModelExecutor {
 			ParameterValueList parameterValues) {
 		loadModel(modelPath);
 		conversionResult = convertDiResource();
+		replaceOpaqueBehaviors();
 		Activity activity = conversionResult.getActivity(name);
 		executeActivity(activity, context, parameterValues);
 		return getTrace();
+	}
+	
+	private void replaceOpaqueBehaviors() {
+		List<ActivityNode> nodesWithBehavior = new ArrayList<ActivityNode>();
+		for (fUML.Syntax.Activities.IntermediateActivities.Activity activity : conversionResult
+				.getAllActivities()) {
+			nodesWithBehavior.addAll(getBehaviorNodes(activity.node));
+		}
+
+		for (ActivityNode node : nodesWithBehavior) {
+			if (node instanceof CallBehaviorAction) {
+				CallBehaviorAction callBehaviorAction = (CallBehaviorAction) node;
+				Behavior behavior = callBehaviorAction.behavior;
+				OpaqueBehavior behaviorReplacement = getExecutionContext()
+						.getOpaqueBehavior(behavior.name);
+				if (behaviorReplacement != null) {
+					callBehaviorAction.behavior = behaviorReplacement;
+				}
+			} else if (node instanceof DecisionNode) {
+				DecisionNode decision = (DecisionNode) node;
+				Behavior behavior = decision.decisionInput;
+				OpaqueBehavior behaviorReplacement = getExecutionContext()
+						.getOpaqueBehavior(behavior.name);
+				if (behaviorReplacement != null) {
+					decision.decisionInput = behaviorReplacement;
+				}
+			}
+		}
+	}
+	
+	private List<ActivityNode> getBehaviorNodes(List<ActivityNode> nodes) {
+		List<ActivityNode> nodesWithBehavior = new ArrayList<ActivityNode>();
+		for (ActivityNode node : nodes) {
+			if (node instanceof CallBehaviorAction) {
+				CallBehaviorAction action = (CallBehaviorAction) node;
+				nodesWithBehavior.add(action);
+			} else if (node instanceof DecisionNode) {
+				DecisionNode decision = (DecisionNode) node;
+				if (decision.decisionInput != null) {
+					nodesWithBehavior.add(decision);
+				}
+			}
+			if (node instanceof StructuredActivityNode) {
+				StructuredActivityNode structurednode = (StructuredActivityNode) node;
+				nodesWithBehavior.addAll(getBehaviorNodes(structurednode.node));
+			}
+		}
+		return nodesWithBehavior;
 	}
 
 	public Trace executeActivity(String name) {
