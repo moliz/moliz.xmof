18bf7295a080c57fd045dd9d67a2d6982a81dabe
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2013-08-02 10:44:30 +0200

    Fixes qualified name conversion for package hierarchy in xMOF

--- org.modelexecution.fuml.convert.xmof/src/org/modelexecution/fuml/convert/xmof/internal/ecore/PackagePopulator.java
+++ org.modelexecution.fuml.convert.xmof/src/org/modelexecution/fuml/convert/xmof/internal/ecore/PackagePopulator.java
@@ -12,15 +12,19 @@ package org.modelexecution.fuml.convert.xmof.internal.ecore;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.EPackage;
+import org.modelexecution.fuml.convert.IConversionResult;
 import org.modelexecution.fuml.convert.impl.ConversionResultImpl;
 import org.modelexecution.fuml.convert.xmof.internal.IElementPopulator;
 
 import fUML.Syntax.Classes.Kernel.Element;
+import fUML.Syntax.Classes.Kernel.NamedElement;
 import fUML.Syntax.Classes.Kernel.Package;
 import fUML.Syntax.Classes.Kernel.PackageableElement;
 
 public class PackagePopulator implements IElementPopulator {
 
+	private IConversionResult result;
+	
 	@Override
 	public void populate(Element fumlElement, EModelElement element,
 			ConversionResultImpl result) {
@@ -38,8 +42,34 @@ public class PackagePopulator implements IElementPopulator {
 
 		for (EPackage eSubPackage : ePackage.getESubpackages()) {
 			umlPackage.addPackagedElement((PackageableElement) result
-					.getFUMLElement(eSubPackage));
+					.getFUMLElement(eSubPackage));			
 		}
-
+		
+		this.result = result;				
+		EPackage rootPackage = getRootPackage(ePackage);
+		updatedQualifiedName(rootPackage);
+	}
+	
+	private EPackage getRootPackage(EPackage ePackage) {
+		EPackage eSuperPackage = ePackage.getESuperPackage();
+		if(eSuperPackage == null) {
+			return ePackage;
+		}
+		return getRootPackage(eSuperPackage);
+	}
+	
+	private void updatedQualifiedName(EPackage ePackage) {		
+		NamedElement fUMLPackage = (NamedElement)result.getFUMLElement(ePackage);
+		fUMLPackage.setName(fUMLPackage.name);		
+		for(EClassifier eClassifier : ePackage.getEClassifiers()) {
+			Element fUMLElement = result.getFUMLElement(eClassifier);
+			if(fUMLElement instanceof NamedElement) {
+				NamedElement fUMLNamedElement = (NamedElement) fUMLElement;
+				fUMLNamedElement.setName(fUMLNamedElement.name);
+			}
+		}
+		for (EPackage eSubPackage : ePackage.getESubpackages()) {
+			updatedQualifiedName(eSubPackage);
+		}
 	}
 }
