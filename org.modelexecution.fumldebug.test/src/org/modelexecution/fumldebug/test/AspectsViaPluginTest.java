f8c812981b8396adc796d2ce28a841d6cc3135f8
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2013-10-25 13:25:23 +0200

    Adds trace test based on moliz jar

--- org.modelexecution.fumldebug.test/src/org/modelexecution/fumldebug/test/AspectsViaPluginTest.java
+++ org.modelexecution.fumldebug.test/src/org/modelexecution/fumldebug/test/AspectsViaPluginTest.java
@@ -10,6 +10,7 @@
 package org.modelexecution.fumldebug.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -21,10 +22,15 @@ import org.modelexecution.fumldebug.core.ExecutionEventListener;
 import org.modelexecution.fumldebug.core.event.ActivityEntryEvent;
 import org.modelexecution.fumldebug.core.event.ActivityExitEvent;
 import org.modelexecution.fumldebug.core.event.Event;
+import org.modelexecution.fumldebug.core.trace.tracemodel.ActivityExecution;
+import org.modelexecution.fumldebug.core.trace.tracemodel.ActivityNodeExecution;
+import org.modelexecution.fumldebug.core.trace.tracemodel.Trace;
 import org.modelexecution.fumldebug.core.util.ActivityFactory;
 
 import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValueList;
+import fUML.Syntax.Actions.IntermediateActions.CreateObjectAction;
 import fUML.Syntax.Activities.IntermediateActivities.Activity;
+import fUML.Syntax.Classes.Kernel.Class_;
 
 /**
  * This test only verifies whether the events (and thus the aspects)
@@ -56,6 +62,42 @@ public class AspectsViaPluginTest implements ExecutionEventListener {
 		assertEquals(activity, ((ActivityEntryEvent)eventlist.get(0)).getActivity());
 		assertTrue(eventlist.get(1) instanceof ActivityExitEvent);
 		assertEquals(activity, ((ActivityExitEvent)eventlist.get(1)).getActivity());		
+	}
+	
+	@Test
+	public void testExecutionTrace() {
+		Class_ class_ = ActivityFactory.createClass("class");
+		Activity activity = ActivityFactory.createActivity("Activity testExecutionTrace");		
+		CreateObjectAction createObjectAction1 = ActivityFactory.createCreateObjectAction(activity, "create object 1", class_);
+		CreateObjectAction createObjectAction2 = ActivityFactory.createCreateObjectAction(activity, "create object 2", class_);
+		ActivityFactory.createControlFlow(activity, createObjectAction1, createObjectAction2);
+		
+		ExecutionContext executionContext = ExecutionContext.getInstance();
+		executionContext.execute(activity, null, new ParameterValueList());
+		
+		assertTrue(eventlist.get(0) instanceof ActivityEntryEvent);
+		int executionId = ((ActivityEntryEvent)eventlist.get(0)).getActivityExecutionID();
+		
+		Trace trace = executionContext.getTrace(executionId);
+		assertNotNull(trace);
+		
+		assertEquals(1, trace.getActivityExecutions().size());
+		ActivityExecution activityExecution = trace.getActivityExecutions().get(0);
+		assertEquals(2, activityExecution.getNodeExecutions().size());
+		
+		assertEquals(1, activityExecution.getNodeExecutionsByNode(createObjectAction1).size());
+		ActivityNodeExecution createObjectAction1Execution = activityExecution.getNodeExecutionsByNode(createObjectAction1).get(0);
+		assertEquals(1, activityExecution.getNodeExecutionsByNode(createObjectAction2).size());
+		ActivityNodeExecution createObjectAction2Execution = activityExecution.getNodeExecutionsByNode(createObjectAction2).get(0);
+		
+		// check logical relationships
+		assertEquals(0, createObjectAction1Execution.getLogicalPredecessor().size());
+		assertEquals(1, createObjectAction1Execution.getLogicalSuccessor().size());
+		assertEquals(createObjectAction2Execution, createObjectAction1Execution.getLogicalSuccessor().get(0));
+		
+		assertEquals(1, createObjectAction2Execution.getLogicalPredecessor().size());
+		assertEquals(createObjectAction1Execution, createObjectAction2Execution.getLogicalPredecessor().get(0));
+		assertEquals(0, createObjectAction2Execution.getLogicalSuccessor().size());		
 	}
 	
 	@Override
