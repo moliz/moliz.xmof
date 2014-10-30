52295fe1f4896c6e2b12046e59b1a567f9bcbfb3
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2014-10-30 19:38:54 +0100

    Adds missing @generated NOT annotation

--- org.modelexecution.fuml.trace.uml2.edit/src/org/modelexecution/fuml/trace/uml2/fuml/Semantics/Classes/Kernel/provider/LinkItemProvider.java
+++ org.modelexecution.fuml.trace.uml2.edit/src/org/modelexecution/fuml/trace/uml2/fuml/Semantics/Classes/Kernel/provider/LinkItemProvider.java
@@ -90,7 +90,7 @@ public class LinkItemProvider extends ExtensionalValueItemProvider {
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
