1f1ce5fc01f7865f52409318d5b41ce4a8effd76
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2015-02-18 17:18:43 +0100

    Fixes fUML semantics concerning activity parameters

--- org.modelexecution.xmof.examples/src/org/modelexecution/xmof/examples/FUMLTest.java
+++ org.modelexecution.xmof.examples/src/org/modelexecution/xmof/examples/FUMLTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -15,8 +16,11 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.modelexecution.fumldebug.core.trace.tracemodel.ActivityExecution;
+import org.modelexecution.fumldebug.core.trace.tracemodel.OutputParameterSetting;
+import org.modelexecution.fumldebug.core.trace.tracemodel.OutputParameterValue;
 import org.modelexecution.fumldebug.core.trace.tracemodel.Trace;
 import org.modelexecution.fumldebug.core.trace.tracemodel.ValueInstance;
+import org.modelexecution.fumldebug.core.trace.tracemodel.ValueSnapshot;
 import org.modelexecution.xmof.configuration.ConfigurationObjectMap;
 import org.modelexecution.xmof.vm.XMOFInstanceMap;
 import org.modelexecution.xmof.vm.XMOFVirtualMachine;
@@ -24,6 +28,7 @@ import org.modelexecution.xmof.vm.util.XMOFUtil;
 
 import fUML.Semantics.Classes.Kernel.ExtensionalValue;
 import fUML.Semantics.Classes.Kernel.FeatureValue;
+import fUML.Semantics.Classes.Kernel.IntegerValue;
 import fUML.Semantics.Classes.Kernel.Link;
 import fUML.Semantics.Classes.Kernel.Object_;
 import fUML.Semantics.Classes.Kernel.Reference;
@@ -32,7 +37,7 @@ import fUML.Semantics.Classes.Kernel.Value;
 import fUML.Semantics.Loci.LociL1.Locus;
 
 public class FUMLTest extends SemanticsTest {
-
+	
 	private static final String FUML_METAMODEL_PATH = "http://www.eclipse.org/uml2/5.0.0/UML";
 	private static final String FUML_CONFIGURATION_PATH = "fuml/fuml.xmof";
 
@@ -48,7 +53,13 @@ public class FUMLTest extends SemanticsTest {
 	private static final String EXECUTION_ENVIRONMENT = "ExecutionEnvironment";
 	private static final String FUNCTION_BEHAVIOR = "FunctionBehavior";
 	private static final String INTEGER_PLUS_FUNCTION_BEHAVIOR_EXECUTION = "IntegerPlusFunctionBehaviorExecution";
-
+	
+	private static final String OPAQUE_ACTION_DO_ACTION = "doAction_OpaqueAction";
+	private static final String MAIN = "main";
+	private static final String MAIN_OUTPUTS = "outputs";
+	private static final String INTEGER_VALUE = "IntegerValue";
+	private static final String INTEGER_VALUE_VALUE = "value_IntegerValue";
+	
 	@BeforeClass
 	public static void collectAllActivities() {
 		SemanticsTest.collectAllActivities(FUML_CONFIGURATION_PATH);
@@ -83,8 +94,7 @@ public class FUMLTest extends SemanticsTest {
 			EObject next = eAllContents.next();
 			if (next.eClass().getName()
 					.equals(INTEGER_PLUS_FUNCTION_BEHAVIOR_EXECUTION))
-				;
-			integerPlusFunctionBehaviorExecution = next;
+				integerPlusFunctionBehaviorExecution = next;
 		}
 		assertNotNull(integerPlusFunctionBehaviorExecution);
 		assertTrue(existsModelElementAtLocus(integerPlusFunctionBehaviorExecution));
@@ -112,10 +122,10 @@ public class FUMLTest extends SemanticsTest {
 	}
 	
 	@Test
-	public void test2_activityExecution() {
+	public void test1_activityExecution() {
 		Trace trace = execute("test/fuml/testmodel.uml", "test/fuml/test1parameter.xmi",
 				true);
-		assertNotNull(getActivityExecution(trace, "main"));
+		assertNotNull(getActivityExecution(trace, MAIN));
 		assertNotNull(getActivityExecution(trace, "execute_Executor"));
 		
 		ActivityExecution createExecution = getActivityExecution(trace, "createExecution_ExecutionFactory");
@@ -133,10 +143,10 @@ public class FUMLTest extends SemanticsTest {
 	}
 
 	@Test
-	public void test3_opaqueActionExecution() {
+	public void test2_opaqueActionExecution() {
 		Trace trace = execute("test/fuml/testmodel.uml", "test/fuml/test2parameter.xmi",
 				true);
-		Set<ActivityExecution> activityExecutions_doAction = getActivityExecutions(trace, "doAction_OpaqueAction");
+		Set<ActivityExecution> activityExecutions_doAction = getActivityExecutions(trace, OPAQUE_ACTION_DO_ACTION);
 		assertEquals(3, activityExecutions_doAction.size());
 		
 		ActivityExecution opaqueAction1Execution = getActivityExecutionForActionExecution(trace, "OpaqueAction1");
@@ -147,9 +157,94 @@ public class FUMLTest extends SemanticsTest {
 		assertTrue(opaqueAction3Execution.isChronologicalSuccessorOf(opaqueAction2Execution));
 	}
 	
+	@Test
+	public void test3_parameters() {
+		Trace trace = execute("test/fuml/testmodel.uml", "test/fuml/test3parameter.xmi",
+				true);
+		ActivityExecution activityExecution_main = getActivityExecution(trace, MAIN);
+		assertNotNull(activityExecution_main);
+		
+		IntegerValue outputvalue1 = new IntegerValue();
+		outputvalue1.value = 1;
+		assertTrue(checkActivityModelOutput(trace, "test3_output1", outputvalue1));
+		
+		IntegerValue outputvalue2 = new IntegerValue();
+		outputvalue2.value = 2;
+		assertTrue(checkActivityModelOutput(trace, "test3_output2", outputvalue2));		
+	}
+	
+	private boolean checkActivityModelOutput(Trace trace,
+			String parameterName, Value... values) {
+		List<Object_> outputValues = getActivityModelOutput(trace, parameterName);
+		if(outputValues.size() != values.length)
+			return false;
+		for(int i=0;i<values.length;++i) {
+			Value expectedValue = values[i];
+			Object_ value = outputValues.get(i);
+			if(!equals(expectedValue, value))
+				return false;
+		}
+		return true;
+	}
+	
+	private List<Object_> getActivityModelOutput(Trace trace, String parameterName) {
+		List<ValueSnapshot> output = getActivityModelOutput(trace);
+		Object_ parameterValue = null;
+		for(ValueSnapshot o : output) {
+			Object_ outputObject = (Object_)o.getRuntimeValue();
+			Object_ parameter = getLinkedObject(trace, outputObject, "parameter_ParameterValue");
+			Value name = getFeatureValue(parameter, "name");
+			if(((StringValue)name).value.equals(parameterName)) {
+				parameterValue = outputObject;
+			}
+		}
+		
+		List<Object_> parameterValueValues = getLinkedObjects(trace, parameterValue, "values_ParameterValue");
+		return parameterValueValues;
+	}
+	
+	private List<ValueSnapshot> getActivityModelOutput(Trace trace) {
+		ActivityExecution activityExecution_main = getActivityExecution(trace, MAIN);
+		List<ValueSnapshot> output = getOutput(activityExecution_main, MAIN_OUTPUTS);
+		return output;
+	}
+	
+	
+	private boolean equals(Value expectedValue, Value value) {
+		if(expectedValue instanceof IntegerValue && value instanceof Object_) {
+			Object_ valueObject = (Object_)value;
+			if(valueObject.types.get(0).name.equals(INTEGER_VALUE)) {
+				IntegerValue v = (IntegerValue)getFeatureValue(valueObject, INTEGER_VALUE_VALUE);
+				return equals((IntegerValue)expectedValue, v);
+			}
+		}
+		return false;
+	}
+	
+	private boolean equals(IntegerValue value1, IntegerValue value2) {
+		return value1.value == value2.value;
+	}
+	
+	private List<ValueSnapshot> getOutput(
+			ActivityExecution activityExecution, String parameterName) {
+		List<ValueSnapshot> values = new ArrayList<ValueSnapshot>();
+		for (OutputParameterSetting output : activityExecution
+				.getActivityOutputs()) {
+			if (output.getParameter().name.equals(parameterName)) {
+				for (OutputParameterValue outputValue : output
+						.getParameterValues()) {
+					ValueSnapshot valueSnapshot = outputValue
+							.getValueSnapshot();
+					values.add(valueSnapshot);
+				}
+			}
+		}
+		return values;
+	}
+	
 	private ActivityExecution getActivityExecutionForActionExecution(Trace trace,
 			String actionName) {
-		Set<ActivityExecution> activityExecutions_doAction = getActivityExecutions(trace, "doAction_OpaqueAction");
+		Set<ActivityExecution> activityExecutions_doAction = getActivityExecutions(trace, OPAQUE_ACTION_DO_ACTION);
 		for(ActivityExecution activityExecution_doAction : activityExecutions_doAction) {
 			Object_ semanticVisitor = getContextObject(activityExecution_doAction);
 			Object_ runtimeModelElement = getLinkedObject(trace, semanticVisitor, "runtimeModelElement");
@@ -173,7 +268,15 @@ public class FUMLTest extends SemanticsTest {
 	
 	private Object_ getLinkedObject(Trace trace, Object_ object_,
 			String associationEnd) {
-		Link link = null;
+		List<Object_> linkedObjects = getLinkedObjects(trace, object_, associationEnd);
+		if(linkedObjects.size() == 1)
+			return linkedObjects.get(0);
+		return null;
+	}
+	
+	private List<Object_> getLinkedObjects(Trace trace, Object_ object_,
+			String associationEnd) {
+		Set<Link> links = new LinkedHashSet<Link>();
 		for (ValueInstance valueInstance : trace.getValueInstances()) {
 			Value runtimeValue = valueInstance.getRuntimeValue();
 			if (runtimeValue instanceof Link) {
@@ -190,19 +293,19 @@ public class FUMLTest extends SemanticsTest {
 					}
 				}
 				if (hasAssociationEnd && linksObject) {
-					link = l;
-					break;
+					links.add(l);
 				}
 			}
 		}
-		if (link != null)
+		List<Object_> linkedObjects = new ArrayList<Object_>();
+		for (Link link : links) {
 			for (FeatureValue featureValue : link.featureValues) {
 				if (featureValue.feature.name.equals(associationEnd)) {
-					return ((Reference) featureValue.values.get(0)).referent;
+					linkedObjects.add(((Reference) featureValue.values.get(0)).referent);
 				}
 			}
-
-		return null;
+		}
+		return linkedObjects;
 	}
 
 	private List<Object_> getLinkedObjects(Link link) {
