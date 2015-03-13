80e2422429805f51334f72079acf98b2def1e02c
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2015-03-13 19:41:36 +0100

    Removes obsolete test case for xmof examples

--- org.modelexecution.xmof.examples/src/org/modelexecution/xmof/examples/TestPlainFuml.java
+++ org.modelexecution.xmof.examples/src/org/modelexecution/xmof/examples/TestPlainFuml.java
@@ -5,7 +5,9 @@ import org.modelexecution.fumldebug.core.ExecutionContext;
 import org.modelexecution.fumldebug.core.util.ActivityFactory;
 
 import fUML.Syntax.Actions.BasicActions.CallBehaviorAction;
+import fUML.Syntax.Actions.IntermediateActions.ValueSpecificationAction;
 import fUML.Syntax.Activities.IntermediateActivities.Activity;
+import fUML.Syntax.Activities.IntermediateActivities.ForkNode;
 
 public class TestPlainFuml {
 
@@ -19,6 +21,18 @@ public class TestPlainFuml {
 		ExecutionContext.getInstance().execute(activity, null, null);
 		
 	
+	}
+	
+	@Test
+	public void test2() {
+		Activity activity = ActivityFactory.createActivity("activity");
+		ValueSpecificationAction valueSpecificationAction = ActivityFactory.createValueSpecificationAction(activity, "vs", 1);
+		ForkNode forkNode = ActivityFactory.createForkNode(activity, "fork");
+		CallBehaviorAction callBehaviorAction = ActivityFactory.createCallBehaviorAction(activity, "call", null, 0, 1);
+		ActivityFactory.createObjectFlow(activity, valueSpecificationAction.result, forkNode);
+		ActivityFactory.createObjectFlow(activity, forkNode, callBehaviorAction.argument.get(0));
+		ExecutionContext.getInstance().execute(activity, null, null);
+		
 	}
 
 }
